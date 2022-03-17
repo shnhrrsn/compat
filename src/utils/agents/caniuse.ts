e66f745de84f150770b51a325326b24c5fd04464
shnhrrsn <shnhrrsn@gmail.com>
2022-03-17 00:33:29 +0000

    refactor(agents): improve `caniuse` agent version ranges

--- src/utils/agents/caniuse.ts
+++ src/utils/agents/caniuse.ts
@@ -1,5 +1,5 @@
 import { agents } from 'caniuse-lite'
-import { Range } from 'semver'
+import { coerce, compare, Range, SemVer } from 'semver'
 import { Agent } from '../agent'
 import { makeAgent } from './makeAgent'
 
@@ -12,6 +12,12 @@ const browserMap: Record<string, string> = {
 	firefox_android: 'and_ff',
 }
 
+type VersionRange = {
+	key: string
+	start: SemVer
+	end: string | null
+}
+
 export default function caniuse(name: string) {
 	const agent = agents[name] ?? agents[browserMap[name]]
 
@@ -20,12 +26,16 @@ export default function caniuse(name: string) {
 	}
 
 	const versions: Agent['versions'] = new Map()
+	const keys = Array.from(Object.keys(agent.release_date))
+		.map(parseVersionRange)
+		.filter(({ start }) => start !== null)
+		.sort(({ start: lhs }, { start: rhs }) => compare(lhs, rhs))
 
-	for (const key of Object.keys(agent.release_date)) {
+	for (const [index, key] of keys.entries()) {
 		try {
-			versions.set(new Range(key.replace(/\-/, ' - ')), {
-				date: agent.release_date[key]!,
-				usage: agent.usage_global[key] ?? null,
+			versions.set(new Range(formatVersionRange(key, keys[index + 1])), {
+				date: agent.release_date[key.key]!,
+				usage: agent.usage_global[key.key] ?? null,
 			})
 		} catch (error) {
 			continue
@@ -33,4 +43,23 @@ export default function caniuse(name: string) {
 	}
 
 	return makeAgent(agent.browser, versions)
+}
+
+function parseVersionRange(key: string): VersionRange {
+	if (!key.includes('-')) {
+		return { key, start: coerce(key)!, end: null }
+	}
+
+	const [start, end] = key.split(/\-/)
+	return { key, start: coerce(start)!, end: end }
+}
+
+function formatVersionRange(key: VersionRange, next?: VersionRange): string {
+	if (key.end) {
+		return `${key.start.format()} - ${key.end}`
+	} else if (!next) {
+		return key.start.format()
+	}
+
+	return `${key.start.format()} < ${next.start.format()}`
 }
