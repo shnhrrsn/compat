9c9c13df9ba2fe4cacd65fa2001180aba56ad30a
shnhrrsn <shnhrrsn@gmail.com>
2022-03-16 22:18:23 -0400

    fix(agents): support node/deno versions

--- src/utils/agents/deno.ts
+++ src/utils/agents/deno.ts
@@ -1,15 +1,19 @@
 import denoVersions from '@/@data/denoVersions'
-import { Range } from 'semver'
+import { coerce, compare, Range, SemVer } from 'semver'
+import { AgentVersion } from '../agent'
 import { makeAgent } from './makeAgent'
 
 export function deno() {
 	return makeAgent(
 		'Deno',
 		new Map(
-			Object.entries(denoVersions).map(([version, date]) => [
-				new Range(version),
-				{ date, usage: null },
-			]),
+			Object.entries(denoVersions)
+				.map(
+					([version, date]) =>
+						[coerce(version), { date, usage: null }] as [SemVer, AgentVersion],
+				)
+				.sort(([lhs], [rhs]) => compare(lhs, rhs))
+				.map(([semver, agentVersion]) => [new Range(semver.format()), agentVersion]),
 		),
 	)
 }
--- src/utils/agents/node.ts
+++ src/utils/agents/node.ts
@@ -1,15 +1,19 @@
 import nodejsVersions from '@/@data/nodejsVersions'
-import { Range } from 'semver'
+import { coerce, compare, Range, SemVer } from 'semver'
+import { AgentVersion } from '../agent'
 import { makeAgent } from './makeAgent'
 
 export default function node() {
 	return makeAgent(
 		'Node.js',
 		new Map(
-			Object.entries(nodejsVersions).map(([version, date]) => [
-				new Range(version),
-				{ date, usage: null },
-			]),
+			Object.entries(nodejsVersions)
+				.map(
+					([version, date]) =>
+						[coerce(version), { date, usage: null }] as [SemVer, AgentVersion],
+				)
+				.sort(([lhs], [rhs]) => compare(lhs, rhs))
+				.map(([semver, agentVersion]) => [new Range(semver.format()), agentVersion]),
 		),
 	)
 }
