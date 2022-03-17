dec26e7a850bc0b0242ae25d0da6611a38629170
shnhrrsn <shnhrrsn@gmail.com>
2022-03-17 01:30:53 +0000

    fix: tweak colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
+// organize-imports-ignore
+import 'normalize.css/normalize.css'
 import '@/styles/global.css'
-import 'normalize.css'
 
 export default function App({ Component, pageProps }: any) {
 	return <Component {...pageProps} />
