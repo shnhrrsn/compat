241474fff871f2543e9a7dadb89414b570c74297
shnhrrsn <shnhrrsn@gmail.com>
2022-03-18 00:27:03 -0400

    fix(formatters): improve accuracy of `formatMacros`

--- src/utils/formatters/formatMacros.ts
+++ src/utils/formatters/formatMacros.ts
@@ -2,20 +2,24 @@ import escapeStringRegexp from 'escape-string-regexp'
 import getAllPages from '../getAllPages'
 
 export default async function formatMacros(content: string): Promise<string> {
-	const pages = await getAllPages()
+	const pages = (await getAllPages()).sort((lhs, rhs) => lhs.length - rhs.length)
 	return content.replace(
-		/\{\{jsxref\(\s*"(.+?)"(?:,\s*"(.+?)")?\s*\)\}\}/g,
+		/\{\{jsxref\(\s*"(.+?)"(?:,\s*"(.+?)")?\s*\)\}\}/gi,
 		formatJsxRef.bind(null, pages),
 	)
 }
 
 function formatJsxRef(pages: string[], _: string, ref: string, title?: string): string {
+	title = title && title.length === 0 ? undefined : title
 	const pathname = escapeStringRegexp(
-		ref.replace('()', '').replace('.prototype.', '.').replace(/\./g, '/'),
+		ref
+			.replace(/\(\)/g, '')
+			.replace(/\/.prototype\./g, '.')
+			.replace(/\./g, '/'),
 	)
-	const pattern = new RegExp(`^\/javascript\/.+?${pathname}$`, 'i')
+	const pattern = new RegExp(`^\/javascript\/.+?\/${pathname}$`, 'i')
 	const href = pages.find(page => pattern.test(page))
-	const content = `<code>${ref ?? title}</code>`
+	const content = `<code>${title ?? ref}</code>`
 
 	if (!href) {
 		console.warn(`Could not find path for jsxref: ${ref}`)
