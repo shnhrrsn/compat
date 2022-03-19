fe24221b841b841a9b8d998b310b08236c9fa892
shnhrrsn <shnhrrsn@gmail.com>
2022-03-19 14:30:23 -0400

    feat(images): add `experimental`, `plus` + `warning` icons

--- src/components/shared/image.tsx
+++ src/components/shared/image.tsx
@@ -4,6 +4,7 @@ import Check from '@/public/images/check.svg'
 import Chrome from '@/public/images/chrome.svg'
 import Deno from '@/public/images/deno.svg'
 import Edge from '@/public/images/edge.svg'
+import Experimental from '@/public/images/experimental.svg'
 import Firefox from '@/public/images/firefox.svg'
 import Flag from '@/public/images/flag.svg'
 import Github from '@/public/images/github.svg'
@@ -15,10 +16,12 @@ import MobileOpera from '@/public/images/mobileopera.svg'
 import MobileSafari from '@/public/images/mobilesafari.svg'
 import Nodejs from '@/public/images/nodejs.svg'
 import Opera from '@/public/images/opera.svg'
+import Plus from '@/public/images/plus.svg'
 import Prefix from '@/public/images/prefix.svg'
 import Safari from '@/public/images/safari.svg'
 import SamsungInternet from '@/public/images/samsung-internet.svg'
 import Times from '@/public/images/times.svg'
+import Warning from '@/public/images/warning.svg'
 
 const ImageSrc = {
 	'android': Android,
@@ -27,6 +30,7 @@ const ImageSrc = {
 	'chrome': Chrome,
 	'deno': Deno,
 	'edge': Edge,
+	'experimental': Experimental,
 	'firefox': Firefox,
 	'flag': Flag,
 	'github': Github,
@@ -38,10 +42,12 @@ const ImageSrc = {
 	'mobilesafari': MobileSafari,
 	'nodejs': Nodejs,
 	'opera': Opera,
+	'plus': Plus,
 	'prefix': Prefix,
 	'safari': Safari,
 	'samsung-internet': SamsungInternet,
 	'times': Times,
+	'warning': Warning,
 }
 
 export type ImageSrc = keyof typeof ImageSrc
