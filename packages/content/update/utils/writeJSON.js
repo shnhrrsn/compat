f0746557f75a3654cf2eff12cad26fe73c9b36e8
shnhrrsn <shnhrrsn@gmail.com>
2022-03-28 19:42:47 -0400

    fix(content): fix mkdir in `writeJSON`

--- packages/content/update/utils/writeJSON.js
+++ packages/content/update/utils/writeJSON.js
@@ -6,7 +6,7 @@ import { promises as fs } from 'fs'
  */
 export default async function writeJSON(url, json) {
 	try {
-		await fs.mkdir(new URL('..', url), { recursive: true })
+		await fs.mkdir(new URL('.', url), { recursive: true })
 	} catch (error) {}
 
 	await fs.writeFile(url, JSON.stringify(json, null, 2))
