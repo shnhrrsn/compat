a5a535f739602c28287f993a7298744aaab7595e
shnhrrsn <shnhrrsn@gmail.com>
2022-04-09 18:38:17 -0400

    fix(agents): fix usage info for mdn versions

--- packages/agents/src/agents/mdn.js
+++ packages/agents/src/agents/mdn.js
@@ -24,16 +24,50 @@ export default function mdn(name) {
 		return undefined
 	}
 
+	const entries = Object.entries(agent)
 	return makeAgent(
 		compatData.browsers[name]?.name ?? name,
 		new Map(
-			Object.entries(agent).map(([key, date]) => [
+			entries.map(([key, date], index) => [
 				new SemVer.Range(key),
 				{
 					date: date,
-					usage: usage[name]?.[key] ?? null,
+					usage: computeUsage(name, key, entries[index - 1]?.[0]),
 				},
 			]),
 		),
 	)
+}
+
+/**
+ * MDN excludes browser releases with the same rendering engine
+ * This calculates usage by including usage for the request version
+ * and all subsequent versions until the next version tracked by MDN
+ *
+ * @param {string} name
+ * @param {string} version
+ * @param {string | undefined} nextVersion
+ */
+function computeUsage(name, version, nextVersion = undefined) {
+	const agent = usage[name]
+
+	if (!agent) {
+		return null
+	}
+
+	nextVersion = nextVersion ?? `${SemVer.major(version) + 1}.0.0`
+	const range = new SemVer.Range(`>=${version} <${nextVersion}`)
+
+	/** @type {number | null} */
+	let totalUsage = null
+
+	for (const [version, usage] of Object.entries(agent)) {
+		if (!range.test(version)) {
+			continue
+		}
+
+		totalUsage = (totalUsage ?? 0) + usage
+	}
+
+	return totalUsage
 }
