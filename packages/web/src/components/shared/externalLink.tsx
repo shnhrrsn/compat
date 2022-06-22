ed4ed41f6b0b35679e5fb31ab050ba0f2b6ec830
shnhrrsn <shnhrrsn@gmail.com>
2022-06-21 22:09:35 -0400

    fix(web): update `ExternalLink` to work with optional href

--- packages/web/src/components/shared/externalLink.tsx
+++ packages/web/src/components/shared/externalLink.tsx
@@ -2,11 +2,15 @@ export default function ExternalLink({
 	children,
 	...props
 }: {
-	href: string
+	href?: string
 	title?: string
 	className?: string
 	children: any
 }) {
+	if (!props.href) {
+		return <span className={props.className}>{children}</span>
+	}
+
 	return (
 		<a {...props} target="_blank" rel="noopener noreferrer">
 			{children}
